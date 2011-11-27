330d8cf7007e6136485eb08aa15609a793399bfb
Kristen McNeely-Shaw <kmcneely@cs.tufts.edu>
2011-11-26 23:49:40 -0500

    Draft of Paillier.py.  Still needs work.

--- lib/Crypto/PublicKey/Paillier.py
+++ lib/Crypto/PublicKey/Paillier.py
@@ -1,9 +1,9 @@
 #
-#   Paillier.py : Paillier encryption/decryption and signatures
+#   PublicKey/Paillier.py : Paillier public key encryption/decryption
 #
 #  Part of the Python Cryptography Toolkit
 #
-#  Written by Kristen McNeely-Shaw based on work of Mike Ivanov
+#  Written in 2011 by Kristen McNeely-Shaw
 #
 # ===================================================================
 # The contents of this file are dedicated to the public domain.  To
@@ -23,152 +23,204 @@
 # SOFTWARE.
 # ===================================================================
 
+"""Paillier public key cryptography algorithms"""
+
 __revision__ = "$Id$"
 
-from Crypto.PublicKey.pubkey import *
+#__all__ = ['generate', 'construct', 'error', 'importKey' ]
+__all__ = ['generate', 'construct']
+
+import sys
+if sys.version_info[0] == 2 and sys.version_info[1] == 1:
+    from Crypto.Util.py21compat import *
+from Crypto.Util.py3compat import *
+
 from Crypto.Util import number
 
-class error (Exception):
-    pass
+class _Paillierobj(pubkey.pubkey):
+    keydata = ['n', 'g', 'l', 'm', 'p', 'q', 'n_sq']
 
-# Generate a Paillier key with N bits
-def generate(bits, randfunc, progress_func=None):
-    """generate(bits:int, randfunc:callable, progress_func:callable)
+    def _encrypt(self, m):
+        if not self.has_private():
+            raise TypeError("No private key")
+        r = _getRandomMult(self)
+        return (pow(self.g, m, self.n_sq)*pow(r, self.n, self.n_sq)) % self.n_sq
+    
+    def _decrypt(self, c):
+        return L(pow(c, self.l, self.n_sq), self.n)*self.m % self.n
 
-    Generate a Paillier key of length 'bits', using 'randfunc' to get
-    random data and 'progress_func', if present, to display
-    the progress of the key generation.
-    """
-    if bits < 1024 or (bits & 0xff) != 0:
-        # pubkey.getStrongPrime doesn't like anything that's not a multiple of 128 and > 512
-        raise ValueError("Paillier modulus length must be a multiple of 256 and > 1024")
-    obj=Paillierobj()
-
-    # Generate the prime factors of n
-    if progress_func:
-        progress_func('p,q\n')
-    p = q = 1L
-    while number.size(p*q) < bits:
-        # There aren't many odd numbers that are a multiple of 256 and > 1024
-        p = pubkey.getStrongPrime(bits>>1, 0, 1e-12, randfunc)
-        q = pubkey.getStrongPrime(bits - (bits>>1), 0, 1e-12, randfunc)
-
-    # p shall be smaller than q (for calc of u)
-    if p > q:
-        (p, q)=(q, p)
-    obj.p = p
-    obj.q = q
-
-# Paillier key generation yields PrivateKey(p,q,n) and PublicKey(n)
-#    where PrivateKey has 
-#       l(ambda)=(p-1)*(q-1) which is ideally 
-#            lcm((p-1),(q-1)) but this bigger one works and 
-#       m(u) is the inverse of l modulo n
-#    and PublicKey has n, n^2, and g=n+1 (or actually random g ...)
-# get g by picking random 0 <= k < n and taking k*n+1
-# where "of length bits" is p of length bits/2 and q of same?
-# so in other words I have ((l, m),(n, n^2, g))
-
-
-
-
-#     # Generate random number g
-#     if progress_func:
-#         progress_func('g\n')
-#     size=bits-1-(ord(randfunc(1)) & 63) # g will be from 1--64 bits smaller than p
-#     if size<1:
-#         size=bits-1
-#     while (1):
-#         obj.g=bignum(getPrime(size, randfunc))
-#         if obj.g < obj.p:
-#             break
-#         size=(size+1) % bits
-#         if size==0:
-#             size=4
-#     # Generate random number x
-#     if progress_func:
-#         progress_func('x\n')
-#     while (1):
-#         size=bits-1-ord(randfunc(1)) # x will be from 1 to 256 bits smaller than p
-#         if size>2:
-#             break
-#     while (1):
-#         obj.x=bignum(getPrime(size, randfunc))
-#         if obj.x < obj.p:
-#             break
-#         size = (size+1) % bits
-#         if size==0:
-#             size=4
-#     if progress_func:
-#         progress_func('y\n')
-#     obj.y = pow(obj.g, obj.x, obj.p)
-#     return obj
-
-def construct(tuple):
-    """construct(tuple:(long,long,long,long,long)))
-             : Paillierobj
-    Construct a Paillier key from a 5-tuple of numbers.
-    """
-
-    obj=Paillierobj()
-    if len(tuple) not in [5]:
-        raise ValueError('argument for construct() wrong length')
-    for i in range(len(tuple)):
-        field = obj.keydata[i]
-        setattr(obj, field, tuple[i])
-    return obj
-
-class Paillierobj(pubkey):
-    #keydata=['p', 'g', 'y', 'x']
-    keydata=['l', 'm', 'n', 'n_sq', 'g']
-    def _encrypt(self, M, K):
-        a=pow(self.g, K, self.p)
-        b=( M*pow(self.y, K, self.p) ) % self.p
-        return ( a,b )
-
-    def _decrypt(self, M):
-        if (not hasattr(self, 'x')):
-            raise TypeError('Private key not available in this object')
-        ax=pow(M[0], self.x, self.p)
-        plaintext=(M[1] * inverse(ax, self.p ) ) % self.p
-        return plaintext
-
-    def _sign(self, M, K):
-        if (not hasattr(self, 'x')):
-            raise TypeError('Private key not available in this object')
-        p1=self.p-1
-        if (GCD(K, p1)!=1):
-            raise ValueError('Bad K value: GCD(K,p-1)!=1')
-        a=pow(self.g, K, self.p)
-        t=(M-self.x*a) % p1
-        while t<0: t=t+p1
-        b=(t*inverse(K, p1)) % p1
-        return (a, b)
-
-    def _verify(self, M, sig):
-        v1=pow(self.y, sig[0], self.p)
-        v1=(v1*pow(sig[0], sig[1], self.p)) % self.p
-        v2=pow(self.g, M, self.p)
-        if v1==v2:
-            return 1
-        return 0
+    def _getRandomMult(self):
+        r = self.p # start while loop
+        while (r % self.p == 0) or (r % self.q == 0):
+            r = number.getRandomRange(1, self.n-1, self._randfunc)
+        return r
 
     def size(self):
-        "Return the maximum number of bits that can be handled by this key."
-        return number.size(self.p) - 1
+        return number.size(self.n) - 1 # TODO: check this
 
     def has_private(self):
-        """Return a Boolean denoting whether the object contains
-        private components."""
-        if hasattr(self, 'x'):
+        if hasattr(self, 'l') and hasattr(self, 'm'):
             return 1
         else:
             return 0
 
-    def publickey(self):
-        """Return a new key object containing only the public information."""
-        return construct((self.p, self.g, self.y))
+    def publickey():
+        return construct((self.n, self.g, self.n_sq))
+    
 
 
-object=Paillierobj
+def generate(bits, randfunc=None, progress_func=None):
+    # TODO: keydata = ['n', 'g', 'l', 'm', 'p', 'q', 'n_sq']
+    obj=Paillierobj()
+    # Generate the prime factors of n
+    if progress_func:
+        progress_func('p,q\n')
+    p = q = 1L
+    assert bits % 2 == 0, "Not an even number of bits"
+    while number.size(p*q) < bits:
+        p = number.getPrime(bits>>1, randfunc)
+        q = number.getPrime(bits>>1, randfunc)
+
+    obj.p = p
+    obj.q = q
+    obj.n = p*q
+    obj.n_sq = n*n
+
+    if progress_func:
+        progress_func('l\n')
+    obj.l = number.LCM(obj.p-1, obj.q-1)
+
+    if progress_func:
+        progress_func('g\n')
+
+    obj.g = obj._getRandomMult()*obj.n+1 # TODO: check
+    gExp = L(pow(obj.g, obj.l, obj.n_sq), obj.n)
+    while not num.GCD(gExp, obj.n) == 1:
+        obj.g = obj._getRandomMult()*obj.n+1 # TODO: check
+        gExp = L(pow(obj.g, obj.l, obj.n_sq), obj.n)
+    obj.m = number.inverse(gExp, obj.n)
+
+    assert bits <= 1+obj.size(), "Generated key is too small"
+
+    return obj
+
+def L(u, n):
+    return (u - 1) // n
+    
+
+# # __revision__ = "$Id$"
+
+# # # Paillier key generation yields PrivateKey(p,q,n) and PublicKey(n)
+# # #    where PrivateKey has 
+# # #       l(ambda)=(p-1)*(q-1) which is ideally 
+# # #            lcm((p-1),(q-1)) but this bigger one works and 
+# # #       m(u) is the inverse of l modulo n
+# # #    and PublicKey has n, n^2, and g=n+1 (or actually random g ...)
+# # # get g by picking random 0 <= k < n and taking k*n+1
+# # # where "of length bits" is p of length bits/2 and q of same?
+# # # so in other words I have ((l, m),(n, n^2, g))
+
+
+
+
+# # #     # Generate random number g
+# # #     if progress_func:
+# # #         progress_func('g\n')
+# # #     size=bits-1-(ord(randfunc(1)) & 63) # g will be from 1--64 bits smaller than p
+# # #     if size<1:
+# # #         size=bits-1
+# # #     while (1):
+# # #         obj.g=bignum(getPrime(size, randfunc))
+# # #         if obj.g < obj.p:
+# # #             break
+# # #         size=(size+1) % bits
+# # #         if size==0:
+# # #             size=4
+# # #     # Generate random number x
+# # #     if progress_func:
+# # #         progress_func('x\n')
+# # #     while (1):
+# # #         size=bits-1-ord(randfunc(1)) # x will be from 1 to 256 bits smaller than p
+# # #         if size>2:
+# # #             break
+# # #     while (1):
+# # #         obj.x=bignum(getPrime(size, randfunc))
+# # #         if obj.x < obj.p:
+# # #             break
+# # #         size = (size+1) % bits
+# # #         if size==0:
+# # #             size=4
+# # #     if progress_func:
+# # #         progress_func('y\n')
+# # #     obj.y = pow(obj.g, obj.x, obj.p)
+# # #     return obj
+
+
+# # def construct(tuple):
+# #     """construct(tuple:(long,long,long,long,long)))
+# #              : Paillierobj
+# #     Construct a Paillier key from a 5-tuple of numbers.
+# #     """
+
+# #     obj=Paillierobj()
+# #     if len(tuple) not in [5]:
+# #         raise ValueError('argument for construct() wrong length')
+# #     for i in range(len(tuple)):
+# #         field = obj.keydata[i]
+# #         setattr(obj, field, tuple[i])
+# #     return obj
+
+# # class Paillierobj(pubkey):
+# #     #keydata=['p', 'g', 'y', 'x']
+# #     keydata=['l', 'm', 'n', 'n_sq', 'g']
+# #     def _encrypt(self, M, K):
+# #         a=pow(self.g, K, self.p)
+# #         b=( M*pow(self.y, K, self.p) ) % self.p
+# #         return ( a,b )
+
+# #     def _decrypt(self, M):
+# #         if (not hasattr(self, 'x')):
+# #             raise TypeError('Private key not available in this object')
+# #         ax=pow(M[0], self.x, self.p)
+# #         plaintext=(M[1] * inverse(ax, self.p ) ) % self.p
+# #         return plaintext
+
+# #     def _sign(self, M, K):
+# #         if (not hasattr(self, 'x')):
+# #             raise TypeError('Private key not available in this object')
+# #         p1=self.p-1
+# #         if (GCD(K, p1)!=1):
+# #             raise ValueError('Bad K value: GCD(K,p-1)!=1')
+# #         a=pow(self.g, K, self.p)
+# #         t=(M-self.x*a) % p1
+# #         while t<0: t=t+p1
+# #         b=(t*inverse(K, p1)) % p1
+# #         return (a, b)
+
+# #     def _verify(self, M, sig):
+# #         v1=pow(self.y, sig[0], self.p)
+# #         v1=(v1*pow(sig[0], sig[1], self.p)) % self.p
+# #         v2=pow(self.g, M, self.p)
+# #         if v1==v2:
+# #             return 1
+# #         return 0
+
+# #     def size(self):
+# #         "Return the maximum number of bits that can be handled by this key."
+# #         return number.size(self.p) - 1
+
+# #     def has_private(self):
+# #         """Return a Boolean denoting whether the object contains
+# #         private components."""
+# #         if hasattr(self, 'x'):
+# #             return 1
+# #         else:
+# #             return 0
+
+# #     def publickey(self):
+# #         """Return a new key object containing only the public information."""
+# #         return construct((self.p, self.g, self.y))
+
+
+# # object=Paillierobj
 
